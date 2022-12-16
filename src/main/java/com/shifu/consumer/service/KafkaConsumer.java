9556d1b524ce1d3624649bac49004aded008e82f
phaneendrareedy12 <33367868+phaneendrareedy12@users.noreply.github.com>
2022-12-16 08:51:17 +0530

    unique event changes

--- src/main/java/com/shifu/consumer/service/KafkaConsumer.java
+++ src/main/java/com/shifu/consumer/service/KafkaConsumer.java
@@ -1,17 +1,25 @@
 package com.shifu.consumer.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shifu.consumer.model.MetricEvent;
 import com.shifu.consumer.repository.MetricEventRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Log4j2
 public class KafkaConsumer {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Autowired
     private MetricEventRepository metricEventRepository;
@@ -22,9 +30,40 @@ public class KafkaConsumer {
             ObjectMapper mapper = new ObjectMapper();
             MetricEvent event = mapper.readValue(metricEvent, MetricEvent.class);
             log.info(event);
-            metricEventRepository.save(event);
+            Optional<MetricEvent> eventPresentInDB = metricEventRepository.findById(event.getDeviceId());
+            if(eventPresentInDB.isPresent()) {
+                if(!checkDeviceInfo(event, eventPresentInDB.get())) {
+                    updateMetricEvent(event);
+                }
+            } else {
+                metricEventRepository.save(event);
+            }
         } catch(Exception e) {
             log.error("Exception while saving kafka data to DB ", e);
         }
+    }
+
+    private boolean checkDeviceInfo(MetricEvent event, MetricEvent eventFromDB) {
+        if(event.getMetrics().size() != eventFromDB.getMetrics().size() || event.getTimeStamps().size() != eventFromDB.getTimeStamps().size())
+            return false;
+        return event.getMetrics().entrySet().stream()
+                .allMatch(e -> e.getValue().equals(eventFromDB.getMetrics().get(e.getKey())))
+                && event.getTimeStamps().entrySet().stream()
+                .allMatch(e -> e.getValue().equals(eventFromDB.getTimeStamps().get(e.getKey())))
+                && event.getCountryCd().equals(eventFromDB.getCountryCd())
+                && event.getSerialNum().equals(eventFromDB.getSerialNum())
+                && event.getSiteNbr() == eventFromDB.getSiteNbr();
+    }
+
+    private void updateMetricEvent(MetricEvent metricEvent) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("deviceId").is(metricEvent.getDeviceId()));
+        Update update = new Update();
+        update.set("metrics", metricEvent.getMetrics());
+        update.set("timeStamps", metricEvent.getTimeStamps());
+        update.set("countryCd", metricEvent.getCountryCd());
+        update.set("siteNbr", metricEvent.getSiteNbr());
+        update.set("serialNum", metricEvent.getSerialNum());
+        mongoTemplate.findAndModify(query, update, MetricEvent.class);
     }
 }
